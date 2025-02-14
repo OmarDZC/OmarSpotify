0978e65d0582975d3e13770fc6ca61af7198c862
Omar <alumnodaw@example.com>
2025-02-14 19:23:18 +0100

    actualizar registro y login

--- OmarProyectoSymfony/src/Entity/Usuario.php
+++ OmarProyectoSymfony/src/Entity/Usuario.php
@@ -198,7 +198,6 @@ class Usuario implements UserInterface, PasswordAuthenticatedUserInterface
     public function getRoles(): array
     {
         $roles = $this->roles;
-        $roles[] = 'ROLE_USER';
         return array_unique($roles);
     }
 
